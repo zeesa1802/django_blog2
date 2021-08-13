989b8cac04bf1d21447cde462367dc90f996598a
Zeeshan <zeesa18.2@gmail.com>
2021-08-13 17:49:42 +0500

    initial commit

---
+++ blog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Author, Post, Tag, Comment
+
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    # readonly_fields = ('slug',)
+    list_filter =  ("author", "tags", "date",)
+    list_display = ("title", "date", "author",)
+    prepopulated_fields = {"slug": ("title",)}
+
+class CommentAdmin(admin.ModelAdmin):
+    # readonly_fields = ('slug',)
+    list_display = ("user_name", "post",)
+
+
+admin.site.register(Author)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
---
+++ blog/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.db.models.deletion import CASCADE, SET_NULL
+from django.utils.text import slugify
+from django.core.validators import MinLengthValidator
+
+# Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+    def __str__(self):
+        return self.caption
+    
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    excerpt = models.CharField(max_length=300)
+    image = models.ImageField(upload_to="posts", null=True)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts", null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=150)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=CASCADE, related_name="comments")
